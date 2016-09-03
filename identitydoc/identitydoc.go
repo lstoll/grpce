6ffa9fc744ba5a40d9d59f5031f6900998d563ce
Lincoln Stoll <lincoln.stoll@gmail.com>
2016-09-03 17:20:47 +0200

    Re-work identity doc to be doc+sig over pkcs7
    Golang has bad pkcs7 support. This is simpler and more reliable.

--- identitydoc/identitydoc.go
+++ identitydoc/identitydoc.go
@@ -2,44 +2,29 @@ package identitydoc
 
 import (
 	"crypto/x509"
+	"encoding/base64"
 	"encoding/json"
-	"encoding/pem"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"time"
-
-	"github.com/fullsailor/pkcs7"
 )
 
-var ErrInvalidDocument = fmt.Errorf("The provided identify document does not match the signature")
+// ErrInvalidDocument represents the failure when the document is not verified
+// by the signature
+var ErrInvalidDocument = errors.New("The provided identify document does not match the signature")
 
 var (
 	// IdentityDocURL is the URL to retrieve the identity doc from
 	IdentityDocURL = "http://169.254.169.254/latest/dynamic/instance-identity/document"
-	// Pkcs7URL is the URL to retrieve the signed identity doc from
-	Pkcs7URL = "http://169.254.169.254/latest/dynamic/instance-identity/pkcs7"
+	// SignatureURL is the URL to retrieve the signed identity doc from
+	SignatureURL = "http://169.254.169.254/latest/dynamic/instance-identity/signature"
 )
 
-var awsPubKey = `-----BEGIN CERTIFICATE-----
-MIIC7TCCAq0CCQCWukjZ5V4aZzAJBgcqhkjOOAQDMFwxCzAJBgNVBAYTAlVTMRkw
-FwYDVQQIExBXYXNoaW5ndG9uIFN0YXRlMRAwDgYDVQQHEwdTZWF0dGxlMSAwHgYD
-VQQKExdBbWF6b24gV2ViIFNlcnZpY2VzIExMQzAeFw0xMjAxMDUxMjU2MTJaFw0z
-ODAxMDUxMjU2MTJaMFwxCzAJBgNVBAYTAlVTMRkwFwYDVQQIExBXYXNoaW5ndG9u
-IFN0YXRlMRAwDgYDVQQHEwdTZWF0dGxlMSAwHgYDVQQKExdBbWF6b24gV2ViIFNl
-cnZpY2VzIExMQzCCAbcwggEsBgcqhkjOOAQBMIIBHwKBgQCjkvcS2bb1VQ4yt/5e
-ih5OO6kK/n1Lzllr7D8ZwtQP8fOEpp5E2ng+D6Ud1Z1gYipr58Kj3nssSNpI6bX3
-VyIQzK7wLclnd/YozqNNmgIyZecN7EglK9ITHJLP+x8FtUpt3QbyYXJdmVMegN6P
-hviYt5JH/nYl4hh3Pa1HJdskgQIVALVJ3ER11+Ko4tP6nwvHwh6+ERYRAoGBAI1j
-k+tkqMVHuAFcvAGKocTgsjJem6/5qomzJuKDmbJNu9Qxw3rAotXau8Qe+MBcJl/U
-hhy1KHVpCGl9fueQ2s6IL0CaO/buycU1CiYQk40KNHCcHfNiZbdlx1E9rpUp7bnF
-lRa2v1ntMX3caRVDdbtPEWmdxSCYsYFDk4mZrOLBA4GEAAKBgEbmeve5f8LIE/Gf
-MNmP9CM5eovQOGx5ho8WqD+aTebs+k2tn92BBPqeZqpWRa5P/+jrdKml1qx4llHW
-MXrs3IgIb6+hUIB+S8dz8/mmO0bpr76RoZVCXYab2CZedFut7qc3WUH9+EUAH5mw
-vSeDCOUMYQR7R9LINYwouHIziqQYMAkGByqGSM44BAMDLwAwLAIUWXBlk40xTwSw
-7HX32MxXYruse9ACFBNGmdX2ZBrVNGrN9N2f6ROk0k9K
------END CERTIFICATE-----`
-
+// InstanceIdentityDocument represents the information containe in an instances
+// identity document
+// http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/ec2-instance-metadata.html
 type InstanceIdentityDocument struct {
 	InstanceID       string    `json:"instanceId"`
 	AccountID        string    `json:"accountId"`
@@ -49,64 +34,45 @@ type InstanceIdentityDocument struct {
 	PendingTime      time.Time `json:"pendingTime"`
 }
 
+// GetDocumentAndSignature will return the document and it's signature from the
+// metadata API
 func GetDocumentAndSignature() (doc, p7sig []byte, err error) {
-	// Load the identity doc & sig
 	doc, err = httpGET(IdentityDocURL, 10)
 	if err != nil {
 		return
 	}
-	p7sig, err = httpGET(Pkcs7URL, 10)
+	p7sig, err = httpGET(SignatureURL, 10)
 	if err != nil {
 		return
 	}
 	return
 }
 
-func VerifyDocumentAndSignature(doc, p7sig []byte) (*InstanceIdentityDocument, error) {
-	if !verifyDocToPKCS7(doc, p7sig) {
+// VerifyDocumentAndSignature will confirm that the document is correct by
+// validating it against the signature and cert for the given region. It will
+// return the parsed document if it's valid, or ErrInvalidDocument if it's not.
+func VerifyDocumentAndSignature(region string, doc, sig []byte) (*InstanceIdentityDocument, error) {
+	c, err := CertForRegion(region)
+	if err != nil {
+		return nil, err
+	}
+
+	ds, err := base64.StdEncoding.DecodeString(string(sig))
+	if err != nil {
+		return nil, err
+	}
+
+	err = c.CheckSignature(x509.SHA256WithRSA, doc, []byte(ds))
+	if err != nil {
 		return nil, ErrInvalidDocument
 	}
 
 	iid := &InstanceIdentityDocument{}
-	err := json.Unmarshal(doc, iid)
+	err = json.Unmarshal(doc, iid)
 	if err != nil {
 		return nil, err
 	}
 	return iid, nil
-}
-
-// verifyDocToPKCS7 takes a instance doc and the pkcs7 signed version
-// and returns true if the signature is correct, and was signed by
-// AWS's public key
-func verifyDocToPKCS7(doc, pkcs7signed []byte) bool {
-	sigPEM := "-----BEGIN PKCS7-----\n" + string(pkcs7signed) + "\n-----END PKCS7-----"
-	sigDecode, _ := pem.Decode([]byte(sigPEM))
-	if sigDecode == nil {
-		return false
-	}
-
-	awsBlock, _ := pem.Decode([]byte(awsPubKey))
-	if awsBlock == nil {
-		panic("failed to parse AWS cert PEM")
-	}
-	awsCert, err := x509.ParseCertificate(awsBlock.Bytes)
-	if err != nil {
-		panic("failed to parse AWS cert PEM")
-	}
-
-	p7, err := pkcs7.Parse(sigDecode.Bytes)
-	if err != nil {
-		return false
-	}
-	p7.Content = doc
-	p7.Certificates = []*x509.Certificate{awsCert}
-
-	err = p7.Verify()
-	if err != nil {
-		return false
-	}
-	// if we made it here the document has a valid signature
-	return true
 }
 
 // httpGET is a ghetto retrying HTTP client. Will return the fetched body
--- identitydoc/identitydoc_test.go
+++ identitydoc/identitydoc_test.go
@@ -1,134 +1,68 @@
 package identitydoc
 
-import "testing"
+import (
+	"fmt"
+	"net/http"
+	"testing"
+)
 
-var instance1Pkcs7 = `MIAGCSqGSIb3DQEHAqCAMIACAQExCzAJBgUrDgMCGgUAMIAGCSqGSIb3DQEHAaCAJIAEggGwewog
-ICJwcml2YXRlSXAiIDogIjE3Mi4zMS41NC4yNDkiLAogICJkZXZwYXlQcm9kdWN0Q29kZXMiIDog
-bnVsbCwKICAiYXZhaWxhYmlsaXR5Wm9uZSIgOiAidXMtZWFzdC0xYSIsCiAgInZlcnNpb24iIDog
-IjIwMTAtMDgtMzEiLAogICJpbnN0YW5jZUlkIiA6ICJpLTBlOTBkNDk0ZWNmMWVhNGJjIiwKICAi
-YmlsbGluZ1Byb2R1Y3RzIiA6IG51bGwsCiAgImluc3RhbmNlVHlwZSIgOiAidDIubWljcm8iLAog
-ICJhY2NvdW50SWQiIDogIjcxMjM0OTg2MDg4MyIsCiAgImltYWdlSWQiIDogImFtaS1mY2UzYzY5
-NiIsCiAgInBlbmRpbmdUaW1lIiA6ICIyMDE2LTA2LTA5VDAyOjI1OjI4WiIsCiAgImFyY2hpdGVj
-dHVyZSIgOiAieDg2XzY0IiwKICAia2VybmVsSWQiIDogbnVsbCwKICAicmFtZGlza0lkIiA6IG51
-bGwsCiAgInJlZ2lvbiIgOiAidXMtZWFzdC0xIgp9AAAAAAAAMYIBGDCCARQCAQEwaTBcMQswCQYD
-VQQGEwJVUzEZMBcGA1UECBMQV2FzaGluZ3RvbiBTdGF0ZTEQMA4GA1UEBxMHU2VhdHRsZTEgMB4G
-A1UEChMXQW1hem9uIFdlYiBTZXJ2aWNlcyBMTEMCCQCWukjZ5V4aZzAJBgUrDgMCGgUAoF0wGAYJ
-KoZIhvcNAQkDMQsGCSqGSIb3DQEHATAcBgkqhkiG9w0BCQUxDxcNMTYwNjA5MDIyNTMyWjAjBgkq
-hkiG9w0BCQQxFgQUYQi1EvdojKr1UkbWb8e3PRfTefEwCQYHKoZIzjgEAwQvMC0CFGUvzOyBtGGl
-a40lrRD8473cC947AhUAsJDK8QzfImuoWdIpfKtatcYKX2kAAAAAAAA=`
+var testSig = `Ob3mEexQi/91fA/HMqS7L1DraJ/8T/lAblai/PrSgx6FMMPpQpi2rftc/iUcs4Uufzq0NjXkwk95
+9cRES6s3T36hWgob/cutg5imhdy5++bymuzE8Z6T35pU3y3kn4eS6Yebna1atVbAFifeAqySGXCZ
+l5+VTbjj/MBI7vB1cEs=`
 
-var instance1Document = `{
-  "privateIp" : "172.31.54.249",
+var testDoc = `{
   "devpayProductCodes" : null,
+  "privateIp" : "172.30.0.208",
   "availabilityZone" : "us-east-1a",
+  "accountId" : "021124591875",
   "version" : "2010-08-31",
-  "instanceId" : "i-0e90d494ecf1ea4bc",
+  "instanceId" : "i-1ddaabe5",
   "billingProducts" : null,
-  "instanceType" : "t2.micro",
-  "accountId" : "712349860883",
-  "imageId" : "ami-fce3c696",
-  "pendingTime" : "2016-06-09T02:25:28Z",
+  "instanceType" : "t2.nano",
+  "pendingTime" : "2016-09-03T15:07:45Z",
   "architecture" : "x86_64",
+  "imageId" : "ami-2d39803a",
   "kernelId" : null,
   "ramdiskId" : null,
   "region" : "us-east-1"
 }`
-
-var instance2Pkcs7 = `MIAGCSqGSIb3DQEHAqCAMIACAQExCzAJBgUrDgMCGgUAMIAGCSqGSIb3DQEHAaCAJIAEggGwewog
-ICJwcml2YXRlSXAiIDogIjE3Mi4zMS41Ni4yNDUiLAogICJkZXZwYXlQcm9kdWN0Q29kZXMiIDog
-bnVsbCwKICAiYXZhaWxhYmlsaXR5Wm9uZSIgOiAidXMtZWFzdC0xYSIsCiAgInZlcnNpb24iIDog
-IjIwMTAtMDgtMzEiLAogICJpbnN0YW5jZUlkIiA6ICJpLTA2NmNmNzA5Mzk3Yjc4N2Q5IiwKICAi
-YmlsbGluZ1Byb2R1Y3RzIiA6IG51bGwsCiAgImluc3RhbmNlVHlwZSIgOiAidDIubWljcm8iLAog
-ICJhY2NvdW50SWQiIDogIjcxMjM0OTg2MDg4MyIsCiAgInBlbmRpbmdUaW1lIiA6ICIyMDE2LTA2
-LTA5VDAyOjM3OjQ1WiIsCiAgImltYWdlSWQiIDogImFtaS1mY2UzYzY5NiIsCiAgImFyY2hpdGVj
-dHVyZSIgOiAieDg2XzY0IiwKICAia2VybmVsSWQiIDogbnVsbCwKICAicmFtZGlza0lkIiA6IG51
-bGwsCiAgInJlZ2lvbiIgOiAidXMtZWFzdC0xIgp9AAAAAAAAMYIBGDCCARQCAQEwaTBcMQswCQYD
-VQQGEwJVUzEZMBcGA1UECBMQV2FzaGluZ3RvbiBTdGF0ZTEQMA4GA1UEBxMHU2VhdHRsZTEgMB4G
-A1UEChMXQW1hem9uIFdlYiBTZXJ2aWNlcyBMTEMCCQCWukjZ5V4aZzAJBgUrDgMCGgUAoF0wGAYJ
-KoZIhvcNAQkDMQsGCSqGSIb3DQEHATAcBgkqhkiG9w0BCQUxDxcNMTYwNjA5MDIzNzUwWjAjBgkq
-hkiG9w0BCQQxFgQUDh1ZkzRJpKAc7tXjV1ClM5vqi6AwCQYHKoZIzjgEAwQvMC0CFF8mMVcMvUnW
-sfF1Z1N9W/O2YgtqAhUAkJpYBglzpwx8u8waHABgBhi+PmoAAAAAAAA=`
-
-var instance2Document = `{
-  "privateIp" : "172.31.56.245",
-  "devpayProductCodes" : null,
-  "availabilityZone" : "us-east-1a",
-  "version" : "2010-08-31",
-  "instanceId" : "i-066cf709397b787d9",
-  "billingProducts" : null,
-  "instanceType" : "t2.micro",
-  "accountId" : "712349860883",
-  "pendingTime" : "2016-06-09T02:37:45Z",
-  "imageId" : "ami-fce3c696",
-  "architecture" : "x86_64",
-  "kernelId" : null,
-  "ramdiskId" : null,
-  "region" : "us-east-1"
-}`
-
-// We can use this for validating against an "invalid cert"
-var comdirectCert = `-----BEGIN CERTIFICATE-----
-MIIFpjCCBI6gAwIBAgIJAIrHIweEGQCQMA0GCSqGSIb3DQEBBQUAMGoxCzAJBgNV
-BAYTAkRFMTIwMAYDVQQKDClTQ0EgRGV1dHNjaGUgUG9zdCBTaWdudHJ1c3QgdW5k
-IERNREEgR21iSDEnMCUGA1UEAwweU2lnbnRydXN0IENFUlQgQ2xhc3MgMiBDQSA3
-OlBOMB4XDTE1MDEyMjEyMDU0OFoXDTE5MDEyMjEyMDU0N1owgekxITAfBgkqhkiG
-9w0BCQEWEmt1bmRlQGNvbWRpcmVjdC5kZTEiMCAGA1UEAwwZY29tZGlyZWN0IEt1
-bmRlbmJldHJldXVuZzELMAkGA1UEBhMCREUxEjAQBgNVBAcMCVF1aWNrYm9ybjEb
-MBkGA1UECAwSU2NobGVzd2lnLUhvbHN0ZWluMRowGAYDVQQKDBFDb21kaXJlY3Qg
-QmFuayBBRzElMCMGA1UECwwcKEUtTWFpbCBHYXRld2F5IENlcnRpZmljYXRlKTEf
-MB0GA1UEBRMWMDAxMDAwMDAwMDAxMjc4NzQ1MDAwMDCCASIwDQYJKoZIhvcNAQEB
-BQADggEPADCCAQoCggEBAMe6DX2MgA0+gp5G1jyUxqyUlczFvI9kM9310LhX8Kbm
-pDDKsi/49Qns3N3lB7Ke6eBZMrSBlXrukf4JJQrJSRRqdneEgam9BRt62hL5njes
-Nz0DKlmw0I3w+Lznh5NfU0N9MXh+0uG1FkMijs+Lj2XlZfapa2rcCGqrBgxJpM24
-+0ZHwL2RAhDJy6D/MLmyBTNY72mzrJ40r0asOT+RPyIoB8Jv/dHAgt37VIHuFf1q
-pilv799Tr6E4XPz6wHq7JUd0VMj7jjhbFKbpfhRIrnNOjIpwxI+77bjdpdc0mlUm
-0pWAfHHzHQ2/3N0u/Prdd9I3250qYBaK+XgzXNdrJsECAwEAAaOCAc0wggHJMB0G
-A1UdDgQWBBRNSggREK1dQKXAxbdIVcXX4+yK+TAOBgNVHQ8BAf8EBAMCBLAwFAYD
-VR0gBA0wCzAJBgcrEgkCAgIBMAwGA1UdEwEB/wQCMAAwHQYDVR0lBBYwFAYIKwYB
-BQUHAwIGCCsGAQUFBwMEMIHHBgNVHR8Egb8wgbwwgbmgR6BFhkNodHRwOi8vd3d3
-LnNpZ250cnVzdC5kZS9jcmwvZHBzaWdudHJ1c3QvbnFzaWcvc3RjZXJ0X2NsYXNz
-Ml9jYTcuY3Jsom6kbDBqMQswCQYDVQQGEwJERTEyMDAGA1UECgwpU0NBIERldXRz
-Y2hlIFBvc3QgU2lnbnRydXN0IHVuZCBETURBIEdtYkgxJzAlBgNVBAMMHlNpZ250
-cnVzdCBDRVJUIENsYXNzIDIgQ0EgNzpQTjBLBggrBgEFBQcBAQQ/MD0wOwYIKwYB
-BQUHMAGGL2h0dHA6Ly9vY3NwLnNpZ250cnVzdC5kZS9vY3NwL2Rwc2lnbnRydXN0
-L25xc2lnMB0GA1UdEQQWMBSBEmt1bmRlQGNvbWRpcmVjdC5kZTAfBgNVHSMEGDAW
-gBS7B4qkUNWEq6OMi8ODXTNQrc/PqjANBgkqhkiG9w0BAQUFAAOCAQEABJbf1fLI
-eJxu8alfqlaKqVDrYXSnAqapbbJInLSD6DlpddaBFDaL7Uul2TyavLWHcViYOpqQ
-z3FonxkVKbAOg7dSbWFIsgD7wJ6j5h+Hcev9n299+z0c5cEvtURBL4zaNOEIgJuh
-TIQAnnS7yBpH9qWMpiCv1wUF1SbQniFLiKGo+pxuddp92JWh0wXCFguf8kjDdhIz
-SoeFoi4ipg+tIKrm7tI79EWlgcQFb+9UBuh1QbadAxJK3ON1D0EjxFvUnDf/CeIa
-+RBz5aLtis1efTPHjk02fAlHaAIgEA+vibz/FMOL3iIjTnMXv1yxwThRrwwKRToa
-0p6VwEd1Ddx7NQ==
------END CERTIFICATE-----`
 
 func TestDocVerification(t *testing.T) {
-	if !verifyDocToPKCS7([]byte(instance1Document), []byte(instance1Pkcs7)) {
-		t.Error("Instance 1 valid docs failed auth")
+	doc, err := VerifyDocumentAndSignature("us-east-1", []byte(testDoc), []byte(testSig))
+	if err != nil {
+		t.Errorf("Error validating document %q", err)
+	}
+	if doc == nil {
+		t.Error("Test document failed auth")
 	}
 
-	if verifyDocToPKCS7([]byte(instance1Document), []byte(instance2Pkcs7)) {
-		t.Error("Instance 1 doc with instance 2 pkcs7 passed auth")
+	mod := testDoc + "lol"
+	doc, err = VerifyDocumentAndSignature("us-east-1", []byte(mod), []byte(testSig))
+	if err != ErrInvalidDocument {
+		t.Error("Invalid document didn't return ErrInvalidDocument")
 	}
-
-	origCert := awsPubKey
-	awsPubKey = comdirectCert
-	if verifyDocToPKCS7([]byte(instance1Document), []byte(instance1Pkcs7)) {
-		t.Error("Instance 1 doc and sig passed with comdirect cert")
+	if doc != nil {
+		t.Error("Invalid, errored document did not return nil")
 	}
-	awsPubKey = origCert
 }
 
-/*func TestHTTPClient(t *testing.T) {
-	instanceDoc, pkcs7 := instance1Document, instance1Pkcs7
+func TestHTTPClient(t *testing.T) {
 	serveMux := http.NewServeMux()
 	serveMux.HandleFunc("/doc", func(w http.ResponseWriter, req *http.Request) {
-		fmt.Fprintf(w, instanceDoc)
+		fmt.Fprintf(w, testDoc)
 	})
 	serveMux.HandleFunc("/pkcs7", func(w http.ResponseWriter, req *http.Request) {
-		fmt.Fprintf(w, pkcs7)
+		fmt.Fprintf(w, testSig)
 	})
 	go http.ListenAndServe("127.0.0.1:15800", serveMux)
-	identityDocURL = "http://127.0.0.1:15800/doc"
-	pkcs7URL = "http://127.0.0.1:15800/pkcs7"
+	IdentityDocURL = "http://127.0.0.1:15800/doc"
+	SignatureURL = "http://127.0.0.1:15800/pkcs7"
 
-}*/
+	doc, sig, err := GetDocumentAndSignature()
+	if err != nil {
+		t.Errorf("Error fetching test doc %q", err)
+	}
+	if string(doc) != testDoc || string(sig) != testSig {
+		t.Errorf("Retrieved doc or sig doesn't match expectation")
+	}
+}
